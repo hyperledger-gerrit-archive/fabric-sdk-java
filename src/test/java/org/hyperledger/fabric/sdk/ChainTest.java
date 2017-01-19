4da958596a049d87c8e88454e9c492bc4bc7d809
Muhammad Altaf <muhammada@fast.au.fujitsu.com>
2017-01-19 17:12:24 +1100

    FAB-1749 Rename Member to User
    As per the SDK Design Specification, we renamed Member to User
    
    Change-Id: Ia405185f5f5b4b81167b57e59053437ce13d5c61
    Signed-off-by: Muhammad Altaf <muhammada@fast.au.fujitsu.com>

--- src/test/java/org/hyperledger/fabric/sdk/ChainTest.java
+++ src/test/java/org/hyperledger/fabric/sdk/ChainTest.java
@@ -44,8 +44,8 @@ public class ChainTest {
 	    try {
 		cop = new MemberServicesCOPImpl("http://localhost:8888", null);
 		testChain.setMemberServices(cop);
-		Member member = getEnrolledMember("admin", "adminpw");
-		testChain.newTransactionContext(member);
+		User user = getEnrolledMember("admin", "adminpw");
+		testChain.newTransactionContext(user);
 
 	    } catch (Exception e) {
 		logger.error("Failed to create COP object");
@@ -84,7 +84,7 @@ public class ChainTest {
     public void testCreateTransactionProposal() throws EnrollmentException {
 	TransactionRequest request = new TransactionRequest();
 	request.setArgs(new ArrayList<>(Arrays.asList("query", "a")));
-	Member admin = getEnrolledMember("admin", "adminpw");
+	User admin = getEnrolledMember("admin", "adminpw");
 
 	Chain myChain = new Chain("mychain");
 	myChain.newTransactionContext(admin);
@@ -121,7 +121,7 @@ public class ChainTest {
 
 	request.setChaincodeLanguage(ChaincodeLanguage.GO_LANG);
 	request.setTxID("txId");
-	Member admin = getEnrolledMember("admin", "adminpw");
+	User admin = getEnrolledMember("admin", "adminpw");
 
 	Chain myChain = new Chain("mychain");
 	myChain.newTransactionContext(admin);
@@ -160,7 +160,7 @@ public class ChainTest {
 
     @Test
     public void testSendProposal() throws EnrollmentException {
-	Member admin = getEnrolledMember("admin", "adminpw");
+	User admin = getEnrolledMember("admin", "adminpw");
 	Chain myChain = new Chain("mychain");
 
 	try {
@@ -196,7 +196,7 @@ public class ChainTest {
 
     @Test
     public void testSendTransaction() throws EnrollmentException, InvalidProtocolBufferException, CryptoException {
-	Member admin = getEnrolledMember("admin", "adminpw");
+	User admin = getEnrolledMember("admin", "adminpw");
 	Chain myChain = new Chain("mychain");
 	ProposalResponse response = ProposalResponse.newBuilder().build();
 	List<ProposalResponse> responses = new ArrayList<ProposalResponse>();
@@ -377,11 +377,11 @@ public class ChainTest {
      * response.getResponse().getStatus()); }
      */
 
-    public static Member getEnrolledMember(String user, String secret) throws EnrollmentException {
-	Member member = testChain.getMember("admin");
-	if (!member.isEnrolled()) {
-	    member = testChain.enroll(user, secret);
+    public static User getEnrolledMember(String userId, String secret) throws EnrollmentException {
+	User user = testChain.getMember("admin");
+	if (!user.isEnrolled()) {
+	    user = testChain.enroll(userId, secret);
 	}
-	return member;
+	return user;
     }
 }
