a85853789ba351ed9caf9476eb8102fed9ba4df1
rickr <cr22rc@gmail.com>
2017-02-15 14:10:07 -0500

    FAB-2256 Fabric update
    Addressed:
    Fabric Protobuf level to: 9a0aedb1055184631170d400b854e8225607e1a7  Sun Feb 19 22:52:40 2017 +0000
    Docker file not needed for GO chain code.
    Go go code tgz needs full GO path
    Fixed transaction id to follow required patten hex hash of nononce and Identity
    Changed deployment pattern to break up Deployment to Install and Instantiate chaincode.
    ChaincodeID needs version and path
    -------------------------------------
    Patches
    Install working
    Some refactoring
    End2end working.
    Bad transaction error handling
    moved up to 50372c16c76fb2bfbafb2f8ffaf085f085b75b1c Feb 18 10:35:40 2017 +0000
    Refactored names to match instantiate and install
    Cleanup
    Add longer wait time for build verify till we fix.
    rebase
    
    Change-Id: Ife630a6f629da63c3946fedab2409cda0d6afe96
    Signed-off-by: rickr <cr22rc@gmail.com>

---
+++ src/test/fabric_test_commitlevel.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#file used for automatic integration build test
+#This should always match what's in the README.md
+export FABRIC_COMMIT=9a0aedb1055184631170d400b854e8225607e1a7
+export FABRIC_CA_COMMIT=c4e83c10292d53fa0f1a1688899a594fe4ece26b
