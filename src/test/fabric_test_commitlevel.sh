ecdf11c51c7338de9d1b0306e6a9c6a65ddc9cfb
rickr <cr22rc@gmail.com>
2017-03-03 21:13:44 -0500

    detTestchain
    new configtx files for docker
    
    Change-Id: Ica63d1ddebb6f6ebbd642598b4aaf96011b9e288
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/test/fabric_test_commitlevel.sh
+++ src/test/fabric_test_commitlevel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #file used for automatic integration build test
 #This should always match what's in the README.md
-export FABRIC_COMMIT=39da6d5f263913668567112399c1708e3f87efc3
+export FABRIC_COMMIT=5f4b99a894826f0e8f2ea1ddcfe27099da5e7760
 export FABRIC_CA_COMMIT=c9fb04e0f795589485915883fcbca3ae2fc0aaad
