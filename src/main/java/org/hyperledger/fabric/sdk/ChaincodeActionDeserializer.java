c2287359fb84e90ba013f18844b6bbb5544ac39d
rickr <cr22rc@gmail.com>
2017-03-07 15:36:30 -0500

    WIP FAB-2681 BlockEvent enhancement
    WIP FAB-2681 BlockEvent return proposal, read write set and result
    
    https://gerrit.hyperledger.org/r/#/c/6969/
    
    Change-Id: I19f992d162eaf519e60a2add0bdb235216f59157
    Signed-off-by: rickr <cr22rc@gmail.com>

---
+++ src/main/java/org/hyperledger/fabric/sdk/ChaincodeActionDeserializer.java
@@ -0,0 +1,74 @@
+/*
+ *
+ *  Copyright 2016,2017 DTCC, Fujitsu Australia Software Technology, IBM - All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.hyperledger.fabric.sdk;
+
+import java.lang.ref.WeakReference;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.hyperledger.fabric.protos.ledger.rwset.Rwset.TxReadWriteSet;
+import org.hyperledger.fabric.sdk.exception.InvalidProtocolBufferRuntimeException;
+
+import static org.hyperledger.fabric.protos.peer.FabricProposal.ChaincodeAction;
+
+class ChaincodeActionDeserializer {
+    private final ByteString byteString;
+    private WeakReference<ChaincodeAction> chaincodeAction;
+
+    ChaincodeActionDeserializer(ByteString byteString) {
+        this.byteString = byteString;
+    }
+
+    ChaincodeAction getChaincodeAction() {
+        ChaincodeAction ret = null;
+
+        if (chaincodeAction != null) {
+            ret = chaincodeAction.get();
+
+        }
+        if (ret == null) {
+
+            try {
+                ret = ChaincodeAction.parseFrom(byteString);
+            } catch (InvalidProtocolBufferException e) {
+                throw new InvalidProtocolBufferRuntimeException(e);
+            }
+
+            chaincodeAction = new WeakReference<>(ret);
+
+        }
+
+        //ret.getResults() rwset
+        //TODO events ?
+        // ret.getEvents() // ?
+        // FabricProposalResponse.Response resp = ret.getResponse();
+
+        return ret;
+
+    }
+
+    TxReadWriteSet getResults() {
+
+        try {
+            return TxReadWriteSet.parseFrom(getChaincodeAction().getResults());
+        } catch (InvalidProtocolBufferException e) {
+            throw new InvalidProtocolBufferRuntimeException(e);
+        }
+
+    }
+
+}
---
+++ src/main/java/org/hyperledger/fabric/sdk/ChaincodeProposalPayloadDeserializer.java
@@ -0,0 +1,63 @@
+/*
+ *
+ *  Copyright 2016,2017 DTCC, Fujitsu Australia Software Technology, IBM - All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.hyperledger.fabric.sdk;
+
+import java.lang.ref.WeakReference;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.hyperledger.fabric.protos.peer.FabricTransaction.TransactionAction;
+import org.hyperledger.fabric.sdk.exception.InvalidProtocolBufferRuntimeException;
+
+import static org.hyperledger.fabric.protos.peer.FabricProposal.ChaincodeProposalPayload;
+
+class ChaincodeProposalPayloadDeserializer {
+    private final ByteString byteString;
+    private WeakReference<ChaincodeProposalPayload> chaincodeProposalPayload;
+
+    ChaincodeProposalPayloadDeserializer(TransactionAction actions) {
+        byteString = actions.toByteString();
+    }
+
+    ChaincodeProposalPayload getChaincodeProposalPayload() {
+        ChaincodeProposalPayload ret = null;
+
+        if (chaincodeProposalPayload != null) {
+            ret = chaincodeProposalPayload.get();
+
+        }
+        if (ret == null) {
+
+            try {
+                ret = ChaincodeProposalPayload.parseFrom(byteString);
+            } catch (InvalidProtocolBufferException e) {
+                throw new InvalidProtocolBufferRuntimeException(e);
+            }
+
+            chaincodeProposalPayload = new WeakReference<>(ret);
+
+        }
+
+        //TODO getinput
+
+        //ret.getInput(); //Chaincode.ChaincodeInput cinput = Chaincode.ChaincodeInput.parseFrom(ccpp.getInput());
+
+        return ret;
+
+    }
+
+}
