30fc5dc72799095c39d9330f4dacc6fc2b3fe70b
tuand27613 <tuan.dang.27613@gmail.com>
2017-03-08 10:55:10 -0500

    Test 400 bad request with latest commits
    Change-Id: I8118c342cb93b674dad8b6b1119f046a4b80983a
    Signed-off-by: tuand27613 <tuan.dang.27613@gmail.com>

--- src/test/fabric_test_commitlevel.sh
+++ src/test/fabric_test_commitlevel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #file used for automatic integration build test
 #This should always match what's in the README.md
-export FABRIC_COMMIT=db2021e3f8c826eb008e72321cfc60e72c93ad5e
+export FABRIC_COMMIT=0fc6c4df6855fed4c9a3c33a2535b0503f056325
 export FABRIC_CA_COMMIT=f18b6b769b80c889cb6b82ce34d755d9303ec881
