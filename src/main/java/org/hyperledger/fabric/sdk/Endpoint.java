8ee5bb8cde1f4be63e9df7d69c5c2b31b944ed24
rickr <cr22rc@gmail.com>
2017-03-14 10:16:44 -0400

    WIP FAB-1732 TLS support
    Change-Id: Iae4315020fe410babe2adcdcda2b1187014353fd
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/main/java/org/hyperledger/fabric/sdk/Endpoint.java
+++ src/main/java/org/hyperledger/fabric/sdk/Endpoint.java
@@ -14,14 +14,35 @@
 
 package org.hyperledger.fabric.sdk;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.WeakHashMap;
+
+import javax.net.ssl.SSLException;
+
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.netty.GrpcSslContexts;
 import io.grpc.netty.NettyChannelBuilder;
 import io.netty.handler.ssl.SslContext;
 import io.netty.util.internal.StringUtil;
-
-import javax.net.ssl.SSLException;
-import java.util.Properties;
+import org.bouncycastle.asn1.x500.AttributeTypeAndValue;
+import org.bouncycastle.asn1.x500.RDN;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x500.style.BCStyle;
+import org.bouncycastle.asn1.x500.style.IETFUtils;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import org.hyperledger.fabric.sdk.exception.CryptoException;
+import org.hyperledger.fabric.sdk.security.CryptoPrimitives;
 
 import static org.hyperledger.fabric.sdk.helper.SDKUtil.parseGrpcUrl;
 
@@ -29,14 +50,96 @@ public class Endpoint {
 
     private final String addr;
     private final int port;
+    private final Properties properties;
     private ManagedChannelBuilder<?> channelBuilder = null;
 
-    public Endpoint(String url, String pem) {
+//    public Endpoint(String url, String pem) {
+//
+//        Properties purl = parseGrpcUrl(url);
+//        String protocol = purl.getProperty("protocol");
+//        this.addr = purl.getProperty("host");
+//        this.port = Integer.parseInt(purl.getProperty("port"));
+//
+//        if (protocol.equalsIgnoreCase("grpc")) {
+//            this.channelBuilder = ManagedChannelBuilder.forAddress(addr, port)
+//                    .usePlaintext(true);
+//        } else if (protocol.equalsIgnoreCase("grpcs")) {
+//            if (StringUtil.isNullOrEmpty(pem)) {
+//                // use root certificate
+//                this.channelBuilder = ManagedChannelBuilder.forAddress(addr, port);
+//            } else {
+//                try {
+//
+//                    /*
+//                    https://groups.google.com/forum/#!topic/grpc-io/0EdIOpTLWb4
+//                    SslCredentialsOptions ssl_opts = {"", "", ""};
+//
+//      Client greeter(grpc::CreateChannel(std::string(argv[2])+":ABCD", SslCredentials(ssl_opts), ChannelArguments()));
+//                     */
+//
+//                    SslContext sslContext = GrpcSslContexts.forClient().trustManager(new java.io.File(pem)).build();
+//                    this.channelBuilder = NettyChannelBuilder.forAddress(addr, port)
+//                            .sslContext(sslContext).overrideAuthority("admin");
+//                } catch (SSLException sslex) {
+//                    throw new RuntimeException(sslex);
+//                }
+//            }
+//        } else {
+//            throw new RuntimeException("invalid protocol: " + protocol);
+//        }
+//    }
+
+   static Map<String, String> cnCache = Collections.synchronizedMap( new HashMap<>());
+
+    public Endpoint(String url, Properties properties) {
+
+        this.properties = properties;
+
+        String pem = null;
+        String cn = null;
 
         Properties purl = parseGrpcUrl(url);
         String protocol = purl.getProperty("protocol");
         this.addr = purl.getProperty("host");
         this.port = Integer.parseInt(purl.getProperty("port"));
+
+        if (properties != null) {
+            if ( "grpcs".equals(protocol)) {
+                try {
+                    pem = properties.getProperty("pemFile");
+                    cn = properties.getProperty("overrideAuthority");
+
+                    if (cn == null && "true".equals(properties.getProperty("certificateOverride"))) {
+
+                       File pemF=  new File(pem);
+                       String cnKey=   pemF.getAbsolutePath() + pemF.length() + pemF.lastModified();
+
+                       cn =  cnCache.get(cnKey);
+                       if(cn == null) {
+                           Path path = Paths.get(pem);
+                           byte[] data = Files.readAllBytes(path);
+
+                           CryptoPrimitives cp = new CryptoPrimitives();
+
+
+                           X500Name x500name = new JcaX509CertificateHolder((X509Certificate) cp.bytesToCertificate(data)).getSubject();
+                           RDN rdn = x500name.getRDNs(BCStyle.CN)[0];
+                           //   cnn =  cn +"";
+                           AttributeTypeAndValue f = rdn.getFirst();
+                           cn = IETFUtils.valueToString(rdn.getFirst().getValue());
+                           cnCache.put(cnKey, cn);
+                       }
+
+
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+            }
+
+        }
+
 
         if (protocol.equalsIgnoreCase("grpc")) {
             this.channelBuilder = ManagedChannelBuilder.forAddress(addr, port)
@@ -47,9 +150,14 @@ public class Endpoint {
                 this.channelBuilder = ManagedChannelBuilder.forAddress(addr, port);
             } else {
                 try {
+
+
                     SslContext sslContext = GrpcSslContexts.forClient().trustManager(new java.io.File(pem)).build();
                     this.channelBuilder = NettyChannelBuilder.forAddress(addr, port)
                             .sslContext(sslContext);
+                    if (cn != null) {
+                        channelBuilder.overrideAuthority(cn);
+                    }
                 } catch (SSLException sslex) {
                     throw new RuntimeException(sslex);
                 }
@@ -57,6 +165,7 @@ public class Endpoint {
         } else {
             throw new RuntimeException("invalid protocol: " + protocol);
         }
+
     }
 
     public ManagedChannelBuilder<?> getChannelBuilder() {
