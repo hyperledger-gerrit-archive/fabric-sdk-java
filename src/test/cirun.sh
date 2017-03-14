9773aaecc6b3d3e15f669bcc6d9200f04559d2fc
rickr <cr22rc@gmail.com>
2017-03-14 10:16:44 -0400

    WIP FAB-1732 TLS support
    https://gerrit.hyperledger.org/r/#/c/7195/
    
    Change-Id: Iae4315020fe410babe2adcdcda2b1187014353fd
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/test/cirun.sh
+++ src/test/cirun.sh
@@ -2,6 +2,12 @@
 #Script for continuous integration run.  Cleanup, start docker containers for fabric and fabric ca
 #Start integration tests.
 # expect WD env set HLJSDK directory.
+
+unset ORG_HYPERLEDGER_FABRIC_SDKTEST_INTEGRATIONTESTS_TLS
+unset ORG_HYPERLEDGER_FABRIC_SDKTEST_INTEGRATIONTESTS_CA_TLS
+export ORG_HYPERLEDGER_FABRIC_SDKTEST_INTEGRATIONTESTS_TLS=true
+export ORG_HYPERLEDGER_FABRIC_SDKTEST_INTEGRATIONTESTS_CA_TLS=--tls.enabled
+
 cd $WD/src/test/fixture/sdkintegration
 rm -rf /tmp/keyValStore*; rm -rf  /tmp/kvs-hfc-e2e ~/test.properties; rm -rf /var/hyperledger/*
 docker-compose up > /tmp/dockerlogfile.log 2>&1 &
