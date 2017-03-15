ab6bd09871ea4d04731ac16ca01c923740a4237e
tuand27613 <tuan.dang.27613@gmail.com>
2017-03-15 15:17:22 -0400

    [FAB-2751] [WIP] End2EndIT with multiple orgs
    WIP as End2EndIT fails.
    
    Change-Id: I55564f29ca78f96e3185873a9edf14572e63c542
    Signed-off-by: tuand27613 <tuan.dang.27613@gmail.com>

--- src/test/java/org/hyperledger/fabric/sdk/testutils/TestConfig.java
+++ src/test/java/org/hyperledger/fabric/sdk/testutils/TestConfig.java
@@ -39,9 +39,10 @@ public class TestConfig {
 
     private static final String PROPBASE = "org.hyperledger.fabric.sdktest.";
 
-
+    private static final String GOSSIPWAITTIME =  PROPBASE + "GossipWaitTime";
     private static final String INVOKEWAITTIME =  PROPBASE + "InvokeWaitTime";
     private static final String DEPLOYWAITTIME =  PROPBASE + "DeployWaitTime";
+    private static final String INTEGRATIONTESTSMSPIDS = PROPBASE +  "integrationTests.mspids";
     private static final String INTEGRATIONTESTPEERS = PROPBASE +  "integrationTests.peers";
     private static final String INTEGRATIONTESTSORDERERS = PROPBASE +  "integrationTests.orderers";
     private static final String INTEGRATIONTESTSEVENTHUBS = PROPBASE +  "integrationTests.eventhubs";
@@ -70,13 +71,15 @@ public class TestConfig {
 
             // Default values
 
+            defaultProperty(GOSSIPWAITTIME, "5000");
             defaultProperty(INVOKEWAITTIME, "100000");
             defaultProperty(DEPLOYWAITTIME, "120000");
-            defaultProperty(INTEGRATIONTESTPEERS, "grpc://localhost:7051,grpc://localhost:7056");
+            defaultProperty(INTEGRATIONTESTSMSPIDS, "peerOrg1 Org1MSP, peerOrg2 Org2MSP");
+            defaultProperty(INTEGRATIONTESTPEERS, "peerOrg1 peer0@grpc://localhost:7051,peerOrg1 peer1@grpc://localhost:7056,peerOrg2 peer2@grpc://localhost:8051,peerOrg2 peer3@grpc://localhost:8056");
             defaultProperty(INTEGRATIONTESTSORDERERS, "grpc://localhost:7050");
 
-            defaultProperty(INTEGRATIONTESTSEVENTHUBS, "grpc://localhost:7053");
-            defaultProperty(INTEGRATIONTESTSFABRICCA, "http://localhost:7054");
+            defaultProperty(INTEGRATIONTESTSEVENTHUBS, "grpc://localhost:7053,grpc://localhost:8053");
+            defaultProperty(INTEGRATIONTESTSFABRICCA, "peerOrg1 http://localhost:7054, peerOrg2 http://localhost:8054");
 
         }
 
@@ -153,6 +156,9 @@ public class TestConfig {
         return Integer.parseInt(getProperty(DEPLOYWAITTIME));
     }
 
+    public String getIntegrationTestsMSPIDs(){
+        return getProperty(INTEGRATIONTESTSMSPIDS);
+    }
 
     public String getIntegrationTestsPeers() {
         return getProperty(INTEGRATIONTESTPEERS);
@@ -170,4 +176,7 @@ public class TestConfig {
         return getProperty(INTEGRATIONTESTSFABRICCA);
     }
 
+    public int getGossipWaitTime() {
+        return Integer.parseInt(getProperty(GOSSIPWAITTIME));
+    }
 }
