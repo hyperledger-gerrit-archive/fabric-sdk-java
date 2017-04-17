de9383789436c2383c1bcba96f410d3d5f268918
rickr <cr22rc@gmail.com>
2017-04-17 19:36:47 -0400

    master
    Change-Id: Ia18e9ffe21991054fd622b045fad45a216983d04
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/test/fabric_test_commitlevel.sh
+++ src/test/fabric_test_commitlevel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #file used for automatic integration build test
 #This should always match what's in the README.md
-export FABRIC_COMMIT=latest
+export FABRIC_COMMIT=7e401a115e3a524c07c07129a7096fd591bada69
 export FABRIC_CA_COMMIT=latest
