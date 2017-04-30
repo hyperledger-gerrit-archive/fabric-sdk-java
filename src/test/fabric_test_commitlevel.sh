24427b9e8712679815d0f8378ff365379a29ef94
rickr <cr22rc@gmail.com>
2017-04-30 09:59:44 -0400

    WIP master check build at readme.md level
    used to just check if current master works with readme.md
    Not to be committed.
    
    Change-Id: Ia18e9ffe21991054fd622b045fad45a216983d04
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/test/fabric_test_commitlevel.sh
+++ src/test/fabric_test_commitlevel.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #file used for automatic integration build test
 #This should always match what's in the README.md
-export FABRIC_COMMIT=latest
-export FABRIC_CA_COMMIT=latest
+export FABRIC_COMMIT=b2a2b3b11481438639bf27ed10b99e490dd23b8c
+export FABRIC_CA_COMMIT=09107e7ba6fef7134c949a8edd5d036f9832398b
