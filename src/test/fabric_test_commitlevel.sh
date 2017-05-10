5f7d576200688fdb0955ea5d977ed8f19c21d944
rickr <cr22rc@gmail.com>
2017-05-10 12:44:26 -0400

    WIP Test master build
    DO NOT DELIVER
    
    Change-Id: Id631cf9268a457a98771b112982b3071a4b9d6af
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/test/fabric_test_commitlevel.sh
+++ src/test/fabric_test_commitlevel.sh
@@ -2,5 +2,5 @@
 #file used for automatic integration build test
 #This should always match what's in the README.md
 
-export FABRIC_COMMIT=latest
-export FABRIC_CA_COMMIT=latest
+export FABRIC_COMMIT=e182390035ec207535c7ba0f5df70dc702057e18
+export FABRIC_CA_COMMIT=73a42156f637702b2ac8d4ceae0d663c4c688ed4
