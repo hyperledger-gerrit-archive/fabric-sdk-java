2c75e86535083b06599b8c41e0efdc52a42a3bf5
Chris Murphy <chrism@fast.au.fujitsu.com>
2017-08-25 22:24:42 +1000

    FAB-5632 Initial WIP commit for Network Config
     . Channel.java - Added some logic re Roles
     . HFClient.java - Added NetworkConfig
     . Peer.java - Added support for Roles
     . TestHFClient - Refactor so as to call setupClient
     . Added new NetworkConfig files
    
    Change-Id: I49617d84bea3b5d5654aa6f7abc5ae880b8d8d56
    Signed-off-by: Chris Murphy <chrism@fast.au.fujitsu.com>

--- src/test/java/org/hyperledger/fabric/sdkintegration/IntegrationSuite.java
+++ src/test/java/org/hyperledger/fabric/sdkintegration/IntegrationSuite.java
@@ -14,6 +14,7 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         End2endIT.class,
         End2endAndBackAgainIT.class,
+        NetworkConfigIT.class,
         UpdateChannelIT.class,
         HFCAClientIT.class
 })
