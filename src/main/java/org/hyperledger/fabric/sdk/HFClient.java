f08f98021fc89c0baa3c1afa13a42812c805d124
rickr <cr22rc@gmail.com>
2017-09-18 15:15:03 -0400

    FAB-6200 Java serialize channels.
    PS2 Check for shutdown channel.
    PS3 Minor cleanup no need for thread local.
    PS4 Remove unnecessary added code.
    PS5 Remove added commented code.
    PS6 Minor testcode change.
    PS7 Refactor cleaner to serialize on client itself.
    PS8 Refactor samplestore to use above.
    PS9 Typo fix.
    PS10 ""
    PS11 make sure stream closed.
    
    Change-Id: I0e379d5dab626a4fd71cb66298612f5611215b78
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/main/java/org/hyperledger/fabric/sdk/HFClient.java
+++ src/main/java/org/hyperledger/fabric/sdk/HFClient.java
@@ -14,6 +14,12 @@
 
 package org.hyperledger.fabric.sdk;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -163,6 +169,61 @@ public class HFClient {
             return newChannel;
 
         }
+
+    }
+
+    /**
+     * Deserialize a channel serialized by {@link Channel#serializeChannel()}
+     *
+     * @param file a file which contains the bytes to be deserialized.
+     * @return A Channel that has not been initialized.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws InvalidArgumentException
+     */
+
+    public Channel deSerializeChannel(File file) throws IOException, ClassNotFoundException, InvalidArgumentException {
+
+        if (null == file) {
+            throw new InvalidArgumentException("File parameter may not be null");
+        }
+
+        return deSerializeChannel(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+    }
+
+    /**
+     * Deserialize a channel serialized by {@link Channel#serializeChannel()}
+     *
+     * @param channelBytes bytes to be deserialized.
+     * @return A Channel that has not been initialized.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     * @throws InvalidArgumentException
+     */
+
+    public Channel deSerializeChannel(byte[] channelBytes) throws IOException, ClassNotFoundException, InvalidArgumentException {
+
+        Channel channel;
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(new ByteArrayInputStream(channelBytes));
+            channel = (Channel) in.readObject();
+            if (null != getChannel(channel.getName())) {
+                throw new InvalidArgumentException(format("Channel %s already exists in the client", channel.getName()));
+            }
+            channel.client = this;
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                // Best effort here.
+                logger.error(e);
+            }
+        }
+
+        return channel;
 
     }
 
