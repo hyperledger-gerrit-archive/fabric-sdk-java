740f15cd40a4a3ca6bf7afc3f49cdee7494af625
Gari Singh <gari.r.singh@gmail.com>
2017-11-06 13:44:00 -0500

    [FAB-6887] Support mutual TLS for peer/orderer
    Adds support for configuring client certificates
    when connecting via TLS to a peer or orderer.
    
    Change-Id: Id9200822f266d79672294180bca34c9b724c9f0d
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>

--- src/main/java/org/hyperledger/fabric/sdk/Endpoint.java
+++ src/main/java/org/hyperledger/fabric/sdk/Endpoint.java
@@ -23,6 +23,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 import java.util.Collections;
 import java.util.HashMap;
@@ -46,7 +47,10 @@ import org.bouncycastle.asn1.x500.RDN;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x500.style.IETFUtils;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.cert.jcajce.JcaX509CertificateHolder;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
 import org.hyperledger.fabric.sdk.security.CryptoPrimitives;
 
 import static org.hyperledger.fabric.sdk.helper.Utils.parseGrpcUrl;
@@ -70,6 +74,10 @@ class Endpoint {
         String sslp = null;
         String nt = null;
         byte[] pemBytes = null;
+        String ckf = null;
+        String ccf = null;
+        X509Certificate[] clientCert = new X509Certificate[]{};
+        PrivateKey clientKey = null;
 
         Properties purl = parseGrpcUrl(url);
         String protocol = purl.getProperty("protocol");
@@ -78,6 +86,12 @@ class Endpoint {
 
         if (properties != null) {
             if ("grpcs".equals(protocol)) {
+                CryptoPrimitives cp;
+                try {
+                    cp = new CryptoPrimitives();
+                } catch(Exception e) {
+                    throw new RuntimeException(e);
+                }
                 if (properties.containsKey("pemFile") && properties.containsKey("pemBytes")) {
                     throw new RuntimeException("Properties \"pemBytes\" and \"pemFile\" can not be both set.");
                 }
@@ -100,8 +114,6 @@ class Endpoint {
 
                             cn = CN_CACHE.get(cnKey);
                             if (cn == null) {
-                                CryptoPrimitives cp = new CryptoPrimitives();
-
                                 X500Name x500name = new JcaX509CertificateHolder((X509Certificate) cp.bytesToCertificate(pemBytes)).getSubject();
                                 RDN rdn = x500name.getRDNs(BCStyle.CN)[0];
                                 cn = IETFUtils.valueToString(rdn.getFirst().getValue());
@@ -116,6 +128,29 @@ class Endpoint {
                     }
                 }
 
+                // check for mutual TLS - both clientKeyFile and clientCertFile must be present
+                if (properties.containsKey("clientKeyFile")||properties.containsKey("clientCertFile")){
+                    ckf = properties.getProperty("clientKeyFile");
+                    ccf = properties.getProperty("clientCertFile");
+                    if ((ckf!=null) && (ccf!=null)){
+                        try {
+                            byte[] certBytes = Files.readAllBytes(Paths.get(properties.getProperty("clientCertFile")));
+                            clientCert = new X509Certificate[]{(X509Certificate) cp.bytesToCertificate(certBytes)};
+                        } catch (Exception e) {
+                            throw new RuntimeException("Failed to parse TLS client certificate", e);
+                        }
+                        try {
+                           PEMParser pem = new PEMParser(Files.newBufferedReader(Paths.get(properties.getProperty("clientKeyFile"))));
+                           PEMKeyPair kp = (PEMKeyPair)pem.readObject();
+                           clientKey = new JcaPEMKeyConverter().getPrivateKey(kp.getPrivateKeyInfo());
+                        } catch (Exception e) {
+                            throw new RuntimeException("Failed to parse TLS client key", e);
+                        }
+                    } else {
+                        throw new RuntimeException("Properties \"clientKeyFile\" and \"clientCertFile\" must both be set or both be null");
+                    }
+
+                }
                 sslp = properties.getProperty("sslProvider");
                 if (sslp == null) {
                     throw new RuntimeException("Property of sslProvider expected");
@@ -155,6 +190,7 @@ class Endpoint {
                         SslContext sslContext = GrpcSslContexts.forClient()
                                 .trustManager(myInputStream)
                                 .sslProvider(sslprovider)
+                                .keyManager(clientKey, clientCert)
                                 .build();
                         this.channelBuilder = NettyChannelBuilder.forAddress(addr, port)
                                 .sslContext(sslContext)
