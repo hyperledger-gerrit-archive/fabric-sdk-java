aefacb802cb0be019ff529af57977ec6190fe207
Angelo De Caro <adc@zurich.ibm.com>
2018-05-22 14:56:21 +0200

    [FAB-6843] Integrating Signing Identity
    This change-set does the following:
    - It integrates the SigningIdentity interface in the
    TransactionContext class.
    A SigningIdentity represents the capability of user to
    sign messages. Indeed, a SigningIdentity instance can be
    obtanied from an instance of the User interface.
    This will simplify the integration of different signing
    algorithms like those offered by idemix.
    
    Change-Id: I78eed75938601d42586ac4988040d92896d73e76
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    Signed-off-by: Manu Drijvers <mdr@zurich.ibm.com>

--- src/main/java/org/hyperledger/fabric/sdk/PeerEventServiceClient.java
+++ src/main/java/org/hyperledger/fabric/sdk/PeerEventServiceClient.java
@@ -371,7 +371,7 @@ class PeerEventServiceClient {
 
                     clientTLSCertificateDigest);
             connectEnvelope(envelope);
-        } catch (CryptoException e) {
+        } catch (Exception e) {
             throw new TransactionException(e);
         }
 
