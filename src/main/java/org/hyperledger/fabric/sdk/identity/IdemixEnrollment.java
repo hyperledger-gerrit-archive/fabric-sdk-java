3ce4b59f652767f33b0f94800f228673a533d34d
Angelo De Caro <adc@zurich.ibm.com>
2018-05-22 16:22:08 +0200

    [FAB-6843] Adding IdemixUser
    This change-set does the following:
    - It introduces an Idemix implementation of the
    User interface called IdemixUser.
    - An IdemixUserStore class have been added
    to load instances of IdemixUser.
    - Support tests
    
    Change-Id: I9b8c4f2aacb132e6cb0b74c0ea3ccbeb61ef63be
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    Signed-off-by: Manu Drijvers (mdr@zurich.ibm.com)

--- src/main/java/org/hyperledger/fabric/sdk/identity/IdemixEnrollment.java
+++ src/main/java/org/hyperledger/fabric/sdk/identity/IdemixEnrollment.java
@@ -2,25 +2,46 @@ package org.hyperledger.fabric.sdk.identity;
 
 import java.security.KeyPair;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 
+import org.apache.milagro.amcl.FP256BN.BIG;
+import org.hyperledger.fabric.protos.idemix.Idemix.CredentialRevocationInformation;
 import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.idemix.IdemixCredential;
+import org.hyperledger.fabric.sdk.idemix.IdemixIssuerPublicKey;
 
 public class IdemixEnrollment implements Enrollment {
+
+
+    protected IdemixIssuerPublicKey ipk;
+    protected PublicKey revocationPk;
+    protected String mspId;
+    protected BIG sk;
+    protected IdemixCredential cred;
+    protected CredentialRevocationInformation cri;
+    protected String ou;
+    protected boolean role;
 
     private KeyPair key;
     private String cert;
 
-    public IdemixEnrollment(KeyPair signingKeyPair, String signedPem) {
-        key = signingKeyPair;
-        this.cert = signedPem;
+    public IdemixEnrollment(IdemixIssuerPublicKey ipk, PublicKey revocationPk, String mspId, BIG sk, IdemixCredential cred, CredentialRevocationInformation cri, String ou, boolean role) {
+        this.ipk = ipk;
+        this.revocationPk = revocationPk;
+        this.mspId = mspId;
+        this.sk = sk;
+        this.cred = cred;
+        this.cri = cri;
+        this.ou = ou;
+        this.role = role;
     }
 
     public PrivateKey getKey() {
-        return key.getPrivate();
+        return null;
     }
 
     public String getCert() {
-        return cert;
+        return null;
     }
 
 }
