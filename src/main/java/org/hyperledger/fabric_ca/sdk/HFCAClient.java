e61d5767d269fcc9b1e4977403be0e8faaa827a2
Angelo De Caro <adc@zurich.ibm.com>
2018-05-22 14:56:21 +0200

    [FAB-6843] Integrating Signing Identity
    This change-set does the following:
    - It integrates the SigningIdentity interface in the
    TransactionContext class.
    A SigningIdentity represents the capability of user to
    sign messages. Indeed, a SigningIdentity instance can be
    obtanied from an instance of the User interface.
    This will simplify the integration of different signing
    algorithms like those offered by idemix.
    
    Change-Id: I78eed75938601d42586ac4988040d92896d73e76
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

--- src/main/java/org/hyperledger/fabric_ca/sdk/HFCAClient.java
+++ src/main/java/org/hyperledger/fabric_ca/sdk/HFCAClient.java
@@ -96,6 +96,7 @@ import org.hyperledger.fabric.sdk.Enrollment;
 import org.hyperledger.fabric.sdk.NetworkConfig;
 import org.hyperledger.fabric.sdk.User;
 import org.hyperledger.fabric.sdk.helper.Utils;
+import org.hyperledger.fabric.sdk.identity.X509Enrollment;
 import org.hyperledger.fabric.sdk.security.CryptoPrimitives;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
 import org.hyperledger.fabric_ca.sdk.exception.AffiliationException;
@@ -482,7 +483,7 @@ public class HFCAClient {
             }
             logger.debug("Enrollment done.");
 
-            return new HFCAEnrollment(keypair, signedPem);
+            return new X509Enrollment(keypair, signedPem);
 
         } catch (EnrollmentException ee) {
             logger.error(format("url:%s, user:%s  error:%s", url, user, ee.getMessage()), ee);
@@ -624,7 +625,7 @@ public class HFCAClient {
             logger.debug(format("[HFCAClient] re-enroll returned pem:[%s]", signedPem));
 
             logger.debug(format("reenroll user %s done.", user.getName()));
-            return new HFCAEnrollment(keypair, signedPem);
+            return new X509Enrollment(keypair, signedPem);
 
         } catch (EnrollmentException ee) {
             logger.error(ee.getMessage(), ee);
