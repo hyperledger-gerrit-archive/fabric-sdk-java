4fc09db1642c4f8ad255996e1dccd40928ba5947
Angelo De Caro <adc@zurich.ibm.com>
2018-05-22 14:56:21 +0200

    [FAB-6843] Integrating Signing Identity
    This change-set does the following:
    - It integrates the SigningIdentity interface in the
    TransactionContext class.
    A SigningIdentity represents the capability of user to
    sign messages. Indeed, a SigningIdentity instance can be
    obtanied from an instance of the User interface.
    This will simplify the integration of different signing
    algorithms like those offered by idemix.
    
    Change-Id: I78eed75938601d42586ac4988040d92896d73e76
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ src/main/java/org/hyperledger/fabric/sdk/identity/IdentityFactory.java
@@ -0,0 +1,19 @@
+package org.hyperledger.fabric.sdk.identity;
+
+import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.User;
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+
+public class IdentityFactory {
+
+    public static SigningIdentity getSigningIdentity(CryptoSuite cryptoSuite, User user) {
+        Enrollment enrollment = user.getEnrollment();
+
+        if (enrollment instanceof X509Enrollment) {
+            return new X509SigningIdentity(cryptoSuite, user);
+        }
+
+        throw new IllegalStateException("Invalid enrollment. Expected X509Enrollment. " + enrollment);
+    }
+
+}
