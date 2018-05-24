58689bd6d81d2acc98b9c6e59abaf8153a1843e5
Manu Drijvers <mdr@zurich.ibm.com>
2018-05-24 11:56:29 +0200

    [FAB-8924] add non-revocation proof to idemix
    This commit extends idemix signatures with proofs of
    non-revocation suporting different revocation algorithms.
    Currently, only a "NO_REVOCATION" algorithm is included,
    but future CRs will extend this with meaningful revocation
    algorithms.
    
    Change-Id: I4ba46b36adb59b1f00ccf3b33c041d367066dfe3
    Signed-off-by: Manu Drijvers <mdr@zurich.ibm.com>

--- src/test/java/org/hyperledger/fabric/sdk/idemix/IdemixTest.java
+++ src/test/java/org/hyperledger/fabric/sdk/idemix/IdemixTest.java
@@ -16,18 +16,37 @@
 
 package org.hyperledger.fabric.sdk.idemix;
 
+import java.security.KeyPair;
+
 import org.apache.milagro.amcl.FP256BN.BIG;
+import org.apache.milagro.amcl.FP256BN.ECP;
+import org.hyperledger.fabric.protos.idemix.Idemix;
 import org.hyperledger.fabric.sdk.exception.CryptoException;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class IdemixTest {
 
     @Test
     public void testIdemix() throws CryptoException {
-        // Choose attribute names and create an issuer key pair
-        String[] attributeNames = {"Attribute1", "Attribute2"};
+        // Test WeakBB
+        // Generate a new key pair
+        WeakBB.KeyPair keypair = WeakBB.weakBBKeyGen();
+        // Random message to sign
+        BIG wbbMessage = IdemixUtils.randModOrder();
+        // Sign the message with keypair secret key
+        ECP wbbSignature = WeakBB.weakBBSign(keypair.getSk(), wbbMessage);
+        // Check the signature with valid PK and valid message
+        assertTrue(WeakBB.weakBBVerify(keypair.getPk(), wbbSignature, wbbMessage));
+        // Try to check a random message
+        assertFalse(WeakBB.weakBBVerify(keypair.getPk(), wbbSignature, IdemixUtils.randModOrder()));
+
+        // Test idemix functionality
+        String[] attributeNames = {"Attr1", "Attr2", "Attr3", "Attr4", "Attr5"};
         IdemixIssuerKey key = new IdemixIssuerKey(attributeNames);
         // check that the issuer public key is valid
         assertTrue(key.getIpk().check());
@@ -35,6 +54,7 @@ public class IdemixTest {
         // Test serialization of issuer public key
         assertTrue(new IdemixIssuerPublicKey(key.getIpk().toProto()).check());
 
+        // Test issuance
         // Choose a user secret key and request a credential
         BIG sk = new BIG(IdemixUtils.randModOrder());
         BIG issuerNonce = new BIG(IdemixUtils.randModOrder());
@@ -51,9 +71,16 @@ public class IdemixTest {
         }
         IdemixCredential c = new IdemixCredential(key, m, attrs);
 
-        // user completes the credential and checks validity
-        //c.complete(randCred);
         assertTrue(c.verify(sk, key.getIpk()));
+
+        // Generate a revocation key pair
+        KeyPair revocationKeyPair = RevocationAuthority.generateLongTermRevocationKey();
+        assertNotNull(revocationKeyPair);
+
+        // Create CRI that contains no revocation mechanism
+        int epoch = 0;
+        BIG[] rhIndex = {new BIG(0)};
+        Idemix.CredentialRevocationInformation cri = RevocationAuthority.createCRI(revocationKeyPair.getPrivate(), rhIndex, epoch, RevocationAlgorithm.ALG_NO_REVOCATION);
 
         // Test serialization of IdemixCredential
         assertTrue(new IdemixCredential(c.toProto()).verify(sk, key.getIpk()));
@@ -61,21 +88,34 @@ public class IdemixTest {
         // Create a new unlinkable pseudonym
         IdemixPseudonym pseudonym = new IdemixPseudonym(sk, key.getIpk());
 
-        // Generate new signature, disclosing no attributes
-        boolean[] disclosure = {false, false};
-        byte[] msg = {1, 2, 3, 4};
-        IdemixSignature sig = new IdemixSignature(c, sk, pseudonym, key.getIpk(), disclosure, msg);
+        // Test signing no disclosure
+        boolean[] disclosure = {false, false, false, false, false};
+        byte[] msg = {1, 2, 3, 4, 5};
+        IdemixSignature signature = new IdemixSignature(c, sk, pseudonym, key.getIpk(), disclosure, msg, 0, cri);
+        assertNotNull(signature);
         // check that the signature is valid
-        assertTrue(sig.verify(disclosure, key.getIpk(), msg, attrs));
+        assertTrue(signature.verify(disclosure, key.getIpk(), msg, attrs, 0, revocationKeyPair.getPublic(), epoch));
 
         // Test serialization of IdemixSignature
-        assertTrue(new IdemixSignature(sig.toProto()).verify(disclosure, key.getIpk(), msg, attrs));
+        assertTrue(new IdemixSignature(signature.toProto()).verify(disclosure, key.getIpk(), msg, attrs, 0, revocationKeyPair.getPublic(), epoch));
 
-        // Generate new signature, disclosing both attributes
-        disclosure = new boolean[]{true, true};
-        sig = new IdemixSignature(c, sk, pseudonym, key.getIpk(), disclosure, msg);
+        // Test signing selective disclosure
+        boolean[] disclosure2 = {false, true, true, true, false};
+        signature = new IdemixSignature(c, sk, pseudonym, key.getIpk(), disclosure2, msg, 0, cri);
+        assertNotNull(signature);
+
         // check that the signature is valid
-        assertTrue(sig.verify(disclosure, key.getIpk(), msg, attrs));
+        assertTrue(signature.verify(disclosure2, key.getIpk(), msg, attrs, 0, revocationKeyPair.getPublic(), epoch));
+
+        // Test signature verification with different disclosure
+        assertFalse(signature.verify(disclosure, key.getIpk(), msg, attrs, 0, revocationKeyPair.getPublic(), epoch));
+
+        // test signature verification with different issuer public key
+        assertFalse(signature.verify(disclosure2, new IdemixIssuerKey(attributeNames).getIpk(), msg, attrs, 0, revocationKeyPair.getPublic(), epoch));
+
+        // test signature verification with different message
+        byte[] msg2 = {1, 1, 1};
+        assertFalse(signature.verify(disclosure2, key.getIpk(), msg2, attrs, 0, revocationKeyPair.getPublic(), epoch));
 
         // Sign a message with respect to a pseudonym
         IdemixPseudonymSignature nymsig = new IdemixPseudonymSignature(sk, pseudonym, key.getIpk(), msg);
