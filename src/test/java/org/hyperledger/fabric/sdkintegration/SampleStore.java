3ee00c185a364375f6c51af0eeaf344617b2d713
Saad Karim <skarim@us.ibm.com>
2018-06-28 16:19:24 -0400

    [FAB-10351] Idemix CA support for java SDK
    Add support for the java SDK to get an idemix credential from fabric CA
    and use that credential to submit a proposal and transaction for the
    end-to-end flow.
    
    Change-Id: Ia56563b35211b7e490a08923c6e45c50204bc9a4
    Signed-off-by: Saad Karim <skarim@us.ibm.com>
    Signed-off-by: Keith Smith <bksmith@us.ibm.com>

--- src/test/java/org/hyperledger/fabric/sdkintegration/SampleStore.java
+++ src/test/java/org/hyperledger/fabric/sdkintegration/SampleStore.java
@@ -56,16 +56,15 @@ public class SampleStore {
     private String file;
     private Log logger = LogFactory.getLog(SampleStore.class);
     private CryptoSuite cryptoSuite;
+    private boolean idemixEnabled = false;
 
     public SampleStore(File file) {
         this.file = file.getAbsolutePath();
     }
 
-    public SampleStore(File file, CryptoSuite cryptoSuite) {
-
-        this.file = file.getAbsolutePath();
-        this.cryptoSuite = cryptoSuite;
-    }
+//    public void setIdemixEnabled(boolean idemixEnabled) {
+//        this.idemixEnabled = idemixEnabled;
+//    }
 
     /**
      * Get the value associated with name.
