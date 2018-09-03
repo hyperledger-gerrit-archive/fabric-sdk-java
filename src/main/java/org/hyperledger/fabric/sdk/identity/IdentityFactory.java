a472993b9ec9f8fbe749899d1dc34b8451297c77
rickr <cr22rc@gmail.com>
2018-09-03 14:34:23 -0400

    WIP FABJ-358 End2endMTIT
    Logs (console output) NEEDS TO BE INSPECTED FOR NO ERRORS OR EXCEPTIONS.
    
    PS
    19 Make test fail when exception happen in worker treads.
    
    Change-Id: I9bdac6f17f2085b07026c7d86906f74114c3305c
    Signed-off-by: rickr <cr22rc@gmail.com>
    Signed-off-by: Rafa Torres <rtm@zurich.ibm.com>

--- src/main/java/org/hyperledger/fabric/sdk/identity/IdentityFactory.java
+++ src/main/java/org/hyperledger/fabric/sdk/identity/IdentityFactory.java
@@ -1,23 +1,28 @@
 package org.hyperledger.fabric.sdk.identity;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.hyperledger.fabric.sdk.Enrollment;
 import org.hyperledger.fabric.sdk.User;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import org.hyperledger.fabric.sdk.transaction.TransactionContext;
 
 public class IdentityFactory {
     private IdentityFactory() {
         // private constructor for utility class
     }
-
+    private static final Log logger = LogFactory.getLog(TransactionContext.class);
     public static SigningIdentity getSigningIdentity(CryptoSuite cryptoSuite, User user) {
         Enrollment enrollment = user.getEnrollment();
 
         try {
             if (enrollment instanceof X509Enrollment) {
+                logger.trace("x509 signing");
                 return new X509SigningIdentity(cryptoSuite, user);
             }
 
             if (enrollment instanceof IdemixEnrollment) {
+                logger.trace("idemix signing");
                 return new IdemixSigningIdentity((IdemixEnrollment) enrollment);
             }
         } catch (Exception e) {
