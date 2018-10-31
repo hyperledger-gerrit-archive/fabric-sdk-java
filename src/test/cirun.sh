019e8d31cb3447d59d812eb2517ec29ab2e6bd40
rameshthoomu <rameshbabu.thoomu@gmail.com>
2018-10-31 14:31:41 -0400

    WIP Update plugin versions
    Change-Id: I26b294356fcd7c15d43105fce39943d121cb299d
    Signed-off-by: rameshthoomu <rameshbabu.thoomu@gmail.com>

--- src/test/cirun.sh
+++ src/test/cirun.sh
@@ -81,6 +81,9 @@ docker images
 docker ps -a
 export ORG_HYPERLEDGER_FABRIC_SDK_DIAGNOSTICFILEDIR=target/diagDump
 export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
-mvn -B clean install -DskipITs=false -Dmaven.test.failure.ignore=false javadoc:javadoc ${ORG_HYPERLEDGER_FABRIC_SDKTEST_ITSUITE}
+mvn --version
+rm -rf target
+mvn -B clean install -U -DskipITs=false -Dmaven.test.failure.ignore=false -Djdk.net.URLClassPath.disableClassPathURLCheck=true javadoc:javadoc ${ORG_HYPERLEDGER_FABRIC_SDKTEST_ITSUITE}
+
 docker images
 docker ps -a
