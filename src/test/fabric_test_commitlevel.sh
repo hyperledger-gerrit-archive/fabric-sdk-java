e56dfebe3384a1a486cbb7a56649b72de12bd7ec
rickr <cr22rc@gmail.com>
2019-01-11 15:00:54 -0500

    FABJ-398  Version 2.0.0 start
    Change-Id: I0f32848e81454558fb3193a7143a1fa2f3c92613
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/test/fabric_test_commitlevel.sh
+++ src/test/fabric_test_commitlevel.sh
@@ -10,5 +10,18 @@
 export FABRIC_COMMIT=latest
 export FABRIC_CA_COMMIT=latest
 # if below are set then fabric/ca won't be built.
+#export FABRIC_NO_BUILD=true
+#export FABRIC_CA_NO_BUILD=true
+
+
+case "$ORG_HYPERLEDGER_FABRIC_SDKTEST_VERSION" in
+"1.0.0" | "1.1.0" )
+echo "NOT DOING BUILD ORG_HYPERLEDGER_FABRIC_SDKTEST_VERSION=$ORG_HYPERLEDGER_FABRIC_SDKTEST_VERSION"
 export FABRIC_NO_BUILD=true
 export FABRIC_CA_NO_BUILD=true
+    ;;
+*)
+unset FABRIC_NO_BUILD
+unset FABRIC_CA_NO_BUILD
+    ;;
+esac
