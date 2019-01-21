27bd004d6de50cb38992af64b598d0ef4335bf3d
rickr <cr22rc@gmail.com>
2019-01-21 10:17:29 -0500

    WIP FABJ-288 lifecycle
    PS
    10 Code works end2end needs Fabric commits.
    11 Reverse unintended changes.
    12 Fix queries
    13 Clean up
    14 Make class name more consistent.
    15 Added namespace query.
    16 Cleanup
    17 Update for chaincode hash
    18 Update to support init on invoke call.
    19 Update to latest fabric changes.
    20 Remove unneeded setting in peer.yaml
    21 Support for QueryApprovalStatus
    22 Fix up validation parameter
    23 Cleanup
    24 Cleanup
    25 Cleanup
    26 Cleanup
    27 Collections fixes.
    28 Minor refactor
    29 Find sequence number.
    30 Parameter checking.
    31 Cleanup Parameter checking.
    32 Cleanup
    33 Cleanup more checks.
    
    Change-Id: I324bf96511cc532aa1f0e53d3dc50a4ea445b686
    Signed-off-by: rickr <cr22rc@gmail.com>

---
+++ src/main/java/org/hyperledger/fabric/sdk/LifecycleInstallChaincodeRequest.java
@@ -0,0 +1,36 @@
+/*
+ *
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * /
+ */
+
+package org.hyperledger.fabric.sdk;
+
+import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
+
+/**
+ * LifecycleInstallChaincodeRequest parameters for installing chaincode with lifecycle
+ */
+public class LifecycleInstallChaincodeRequest extends LifecycleRequest {
+
+    private LifecycleChaincodePackage lifecycleChaincodePackage;
+
+    LifecycleInstallChaincodeRequest(User userContext) {
+        super(userContext, false);
+    }
+
+    public LifecycleChaincodePackage getLifecycleChaincodePackage() {
+        return lifecycleChaincodePackage;
+    }
+
+    public void setLifecycleChaincodePackage(LifecycleChaincodePackage lifecycleChaincodePackage) throws InvalidArgumentException {
+
+        if (null == lifecycleChaincodePackage) {
+            throw new InvalidArgumentException("The parameter lifecycleChaincodePackage can not be null.");
+
+        }
+        this.lifecycleChaincodePackage = lifecycleChaincodePackage;
+    }
+}
