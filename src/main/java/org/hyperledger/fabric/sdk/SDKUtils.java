1800731f475d4bca4fa909a6d9e3a9942f10759e
rickr <cr22rc@gmail.com>
2019-01-21 10:17:29 -0500

    WIP FAB-288 lifecycle
    Change-Id: I324bf96511cc532aa1f0e53d3dc50a4ea445b686
    Signed-off-by: rickr <cr22rc@gmail.com>

--- src/main/java/org/hyperledger/fabric/sdk/SDKUtils.java
+++ src/main/java/org/hyperledger/fabric/sdk/SDKUtils.java
@@ -17,24 +17,33 @@
 package org.hyperledger.fabric.sdk;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import com.google.gson.Gson;
 import com.google.protobuf.ByteString;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bouncycastle.asn1.ASN1Integer;
 import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.DERSequenceGenerator;
+import org.hyperledger.fabric.protos.peer.Chaincode;
 import org.hyperledger.fabric.sdk.exception.InvalidArgumentException;
 import org.hyperledger.fabric.sdk.helper.Utils;
 import org.hyperledger.fabric.sdk.security.CryptoSuite;
 
 import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class SDKUtils {
     private static final Log logger = LogFactory.getLog(SDKUtils.class);
@@ -176,5 +185,191 @@ public class SDKUtils {
 
         return ret.values();
 
+    }
+
+    //{"Path":"github.com/hyperledger/fabric/integration/chaincode/simple/cmd","Type":"golang"}[
+
+    public static byte[] generatePlusLifeCycleChaincodeDataPackageBytes(File chaincodeSource, TransactionRequest.Type chaincodeType, File chaincodeMetaInfLocation, String chaincodePath) throws IOException {
+
+        byte[] mataDataBytes = generatePackageMataDataBytes(chaincodePath, chaincodeType);
+        byte[] dataBytes = generatePackageDataBytes(chaincodeSource, chaincodeMetaInfLocation, chaincodeType, chaincodePath);
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(500000);
+
+        // String sourcePath = sourceDirectory.getAbsolutePath();
+
+        TarArchiveOutputStream archiveOutputStream = new TarArchiveOutputStream(new GzipCompressorOutputStream(bos));
+        archiveOutputStream.setLongFileMode(TarArchiveOutputStream.LONGFILE_GNU);
+
+        TarArchiveEntry archiveEntry = new TarArchiveEntry("Chaincode-Package-Metadata.json");
+        archiveEntry.setMode(0100644);
+        archiveEntry.setSize(mataDataBytes.length);
+        archiveOutputStream.putArchiveEntry(archiveEntry);
+        archiveOutputStream.write(mataDataBytes);
+        archiveOutputStream.closeArchiveEntry();
+
+        archiveEntry = new TarArchiveEntry(chaincodeType.toPackageName().toUpperCase() + "-Code-Package.tar.gz");
+        archiveEntry.setMode(0100644);
+        archiveEntry.setSize(dataBytes.length);
+        archiveOutputStream.putArchiveEntry(archiveEntry);
+        archiveOutputStream.write(dataBytes);
+        archiveOutputStream.closeArchiveEntry();
+        archiveOutputStream.close();
+
+        return bos.toByteArray();
+    }
+
+    public static void generatePlusLifeCycleChaincodeDataPackageFile(File outputFile, File chaincodeSource, TransactionRequest.Type chaincodeType, File chaincodeMetaInfLocation, String chaincodePath) throws IOException {
+
+        byte[] bytes = generatePlusLifeCycleChaincodeDataPackageBytes(chaincodeSource, chaincodeType, chaincodeMetaInfLocation, chaincodePath);
+        FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+        fileOutputStream.write(bytes);
+        fileOutputStream.close();
+
+    }
+
+    private static byte[] generatePackageMataDataBytes(String path, TransactionRequest.Type type) {
+
+        return ("{\"Path\":" + new Gson().toJson(path) + ",\"Type\":\"" + type.toPackageName() + "\"}").getBytes(UTF_8); // simple string now.
+
+    }
+
+    private static byte[] generatePackageDataBytes(File chaincodeSource, File chaincodeMetaInfLocation, TransactionRequest.Type chaincodeLanguage, String chaincodePath) throws IOException {
+        logger.debug("createNetModeTransaction");
+
+        if (null == chaincodeSource) {
+            throw new IllegalArgumentException("Missing chaincodeSource or chaincodeInputStream in InstallRequest");
+        }
+
+        final Chaincode.ChaincodeSpec.Type ccType;
+        File projectSourceDir = null;
+        String targetPathPrefix = null;
+        String dplang;
+
+        File metainf = null;
+        if (null != chaincodeMetaInfLocation) {
+            if (!chaincodeMetaInfLocation.exists()) {
+                throw new IllegalArgumentException(format("Directory to find chaincode META-INF %s does not exist", chaincodeMetaInfLocation.getAbsolutePath()));
+            }
+
+            if (!chaincodeMetaInfLocation.isDirectory()) {
+                throw new IllegalArgumentException(format("Directory to find chaincode META-INF %s is not a directory", chaincodeMetaInfLocation.getAbsolutePath()));
+            }
+            metainf = new File(chaincodeMetaInfLocation, "META-INF");
+            logger.trace("META-INF directory is " + metainf.getAbsolutePath());
+            if (!metainf.exists()) {
+
+                throw new IllegalArgumentException(format("The META-INF directory does not exist in %s", chaincodeMetaInfLocation.getAbsolutePath()));
+            }
+
+            if (!metainf.isDirectory()) {
+                throw new IllegalArgumentException(format("The META-INF in %s is not a directory.", chaincodeMetaInfLocation.getAbsolutePath()));
+            }
+            File[] files = metainf.listFiles();
+
+            if (files == null) {
+                throw new IllegalArgumentException("null for listFiles on: " + chaincodeMetaInfLocation.getAbsolutePath());
+            }
+
+            if (files.length < 1) {
+
+                throw new IllegalArgumentException(format("The META-INF directory %s is empty.", metainf.getAbsolutePath()));
+            }
+
+            logger.trace(format("chaincode META-INF found %s", metainf.getAbsolutePath()));
+
+        }
+
+        switch (chaincodeLanguage) {
+            case GO_LANG:
+
+                // chaincodePath is mandatory
+                // chaincodeSource may be a File or InputStream
+
+                //   Verify that chaincodePath is being passed
+                if (Utils.isNullOrEmpty(chaincodePath)) {
+                    throw new IllegalArgumentException("Missing chaincodePath in InstallRequest");
+                }
+
+                dplang = "Go";
+                ccType = Chaincode.ChaincodeSpec.Type.GOLANG;
+                if (null != chaincodeSource) {
+
+                    projectSourceDir = Paths.get(chaincodeSource.toString(), "src", chaincodePath).toFile();
+                    targetPathPrefix = Paths.get("src", chaincodePath).toString();
+                }
+                break;
+
+            case JAVA:
+
+                // chaincodePath is not applicable and must be null
+                // chaincodeSource may be a File or InputStream
+
+                //   Verify that chaincodePath is null
+                if (!Utils.isNullOrEmpty(chaincodePath)) {
+                    throw new IllegalArgumentException("chaincodePath must be null for Java chaincode");
+                }
+
+                dplang = "Java";
+                ccType = Chaincode.ChaincodeSpec.Type.JAVA;
+                if (null != chaincodeSource) {
+                    targetPathPrefix = "src";
+                    projectSourceDir = Paths.get(chaincodeSource.toString()).toFile();
+                }
+                break;
+
+            case NODE:
+
+                // chaincodePath is not applicable and must be null
+                // chaincodeSource may be a File or InputStream
+
+                //   Verify that chaincodePath is null
+                if (!Utils.isNullOrEmpty(chaincodePath)) {
+                    throw new IllegalArgumentException("chaincodePath must be null for Node chaincode");
+                }
+
+                dplang = "Node";
+                ccType = Chaincode.ChaincodeSpec.Type.NODE;
+                if (null != chaincodeSource) {
+
+                    projectSourceDir = Paths.get(chaincodeSource.toString()).toFile();
+                    targetPathPrefix = "src"; //Paths.get("src", chaincodePath).toString();
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected chaincode language: " + chaincodeLanguage);
+        }
+
+        byte[] data = null;
+        //    String chaincodeID = chaincodeName + "::" + chaincodePath + "::" + chaincodeVersion;
+
+        if (chaincodeSource != null) {
+            if (!projectSourceDir.exists()) {
+                final String message = "The project source directory does not exist: " + projectSourceDir.getAbsolutePath();
+                logger.error(message);
+                throw new IllegalArgumentException(message);
+            }
+            if (!projectSourceDir.isDirectory()) {
+                final String message = "The project source directory is not a directory: " + projectSourceDir.getAbsolutePath();
+                logger.error(message);
+                throw new IllegalArgumentException(message);
+            }
+
+//            logger.info(format("Installing '%s' language %s chaincode from directory: '%s' with source location: '%s'. chaincodePath:'%s'",
+//                    chaincodeID, dplang, projectSourceDir.getAbsolutePath(), targetPathPrefix, chaincodePath));
+
+            // generate chaincode source tar
+            data = Utils.generateTarGz(projectSourceDir, targetPathPrefix, metainf);
+
+//            if (null != diagnosticFileDumper) {
+//
+//                logger.trace(format("Installing '%s' language %s chaincode from directory: '%s' with source location: '%s'. chaincodePath:'%s' tar file dump %s",
+//                        chaincodeID, dplang, projectSourceDir.getAbsolutePath(), targetPathPrefix,
+//                        chaincodePath, diagnosticFileDumper.createDiagnosticTarFile(data)));
+//            }
+
+        }
+
+        return data;
     }
 }
