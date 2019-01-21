aacb30622960e31b7e553792803997bf623cc35d
rickr <cr22rc@gmail.com>
2019-01-21 10:17:29 -0500

    WIP FABJ-288 lifecycle
    PS
    10 Code works end2end needs Fabric commits.
    11 Reverse unintended changes.
    12 Fix queries
    13 Clean up
    14 Make class name more consistent.
    15 Added namespace query.
    16 Cleanup
    17 Update for chaincode hash
    18 Update to support init on invoke call.
    19 Update to latest fabric changes.
    20 Remove unneeded setting in peer.yaml
    21 Support for QueryApprovalStatus
    22 Fix up validation parameter
    23 Cleanup
    24 Cleanup
    
    Change-Id: I324bf96511cc532aa1f0e53d3dc50a4ea445b686
    Signed-off-by: rickr <cr22rc@gmail.com>

---
+++ src/main/java/org/hyperledger/fabric/sdk/LifecycleQueryInstalledChaincodeProposalResponse.java
@@ -0,0 +1,80 @@
+/*
+ *
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * /
+ */
+
+package org.hyperledger.fabric.sdk;
+
+import com.google.protobuf.ByteString;
+import org.hyperledger.fabric.protos.peer.FabricProposalResponse;
+import org.hyperledger.fabric.protos.peer.lifecycle.Lifecycle;
+import org.hyperledger.fabric.sdk.exception.ProposalException;
+import org.hyperledger.fabric.sdk.transaction.TransactionContext;
+
+import static java.lang.String.format;
+
+public class LifecycleQueryInstalledChaincodeProposalResponse extends ProposalResponse {
+    public LifecycleQueryInstalledChaincodeProposalResponse(TransactionContext transactionContext, int status, String message) {
+        super(transactionContext, status, message);
+    }
+
+    Lifecycle.QueryInstalledChaincodeResult queryChaincodeDefinitionResult;
+
+    private Lifecycle.QueryInstalledChaincodeResult parsePayload() throws ProposalException {
+
+        if (null == queryChaincodeDefinitionResult) {
+
+            if (getStatus() != Status.SUCCESS) {
+                throw new ProposalException(format("Fabric response failed on peer %s  %s", getPeer(), getMessage()));
+            }
+
+            FabricProposalResponse.ProposalResponse fabricResponse = getProposalResponse();
+
+            //   getChaincodeActionResponsePayload()
+
+            if (null == fabricResponse) {
+                throw new ProposalException(format("Proposal has no Fabric response. %s", getPeer()));
+            }
+
+            ByteString payload = fabricResponse.getResponse().getPayload();
+
+            if (payload == null) {
+                throw new ProposalException(format("Fabric response has no payload  %s", getPeer()));
+            }
+
+            try {
+                queryChaincodeDefinitionResult = Lifecycle.QueryInstalledChaincodeResult.parseFrom(payload);
+            } catch (Exception e) {
+                throw new ProposalException(format("Failure on peer %s %s", getPeer(), e.getMessage()), e);
+            }
+        }
+
+        return queryChaincodeDefinitionResult;
+    }
+
+    public String getPackageId() throws ProposalException {
+
+        Lifecycle.QueryInstalledChaincodeResult queryInstalledChaincodeResult = parsePayload();
+
+        if (queryInstalledChaincodeResult == null) {
+            return null;
+        }
+        return queryInstalledChaincodeResult.getPackageId();
+
+    }
+
+    public String getLabel() throws ProposalException {
+
+        Lifecycle.QueryInstalledChaincodeResult queryInstalledChaincodeResult = parsePayload();
+
+        if (queryInstalledChaincodeResult == null) {
+            return null;
+        }
+        return queryInstalledChaincodeResult.getLabel();
+
+    }
+
+}
