dcb7c8ae1f29db148bb2dcd1caf2ff9bf2f93bd4
Mark S. Lewis <mark_lewis@uk.ibm.com>
2019-09-20 16:11:16 +0100

    FABJ-483: Update dependency in test chaincode
    Update the dependency on com.github.johnrengelman.shadow in the
    test chaincode to match a gradle upgrade in the chaincode
    container.
    
    Also try to fix the exit code on build failure so builds are
    correctly marked as failed if they do not pass.
    
    Change-Id: I28aa6daccfa2aa61fa44206e3437489511120258
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- src/test/cirun.sh
+++ src/test/cirun.sh
@@ -128,6 +128,6 @@ docker ps -a
 export ORG_HYPERLEDGER_FABRIC_SDK_DIAGNOSTICFILEDIR=target/diagDump
 #export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -DforkCount=0"
 export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
-mvn -B clean install -DskipITs=false -Dmaven.test.failure.ignore=false javadoc:javadoc
+mvn -B clean install -DskipITs=false -Dmaven.test.failure.ignore=false javadoc:javadoc || exit $?
 docker images
 docker ps -a
